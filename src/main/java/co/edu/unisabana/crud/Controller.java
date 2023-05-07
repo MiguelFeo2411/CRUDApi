d50916772090ef6d274e0df6f76bf86d5b50fc63
Miguel Feo <mfemerchan@gmail.com>
2023-05-06 23:14:57 -0500

    Crear Controlador con metodo para ver todos los estudiantes

--- src/main/java/co/edu/unisabana/crud/Controller.java
+++ src/main/java/co/edu/unisabana/crud/Controller.java
@@ -1,2 +1,20 @@
-package co.edu.unisabana.crud;public class Controller {
+package co.edu.unisabana.crud;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class Controller {
+    List<Estudiante> estudiantes = new ArrayList<>();
+
+
+
+    @GetMapping(path = "/estudiantes/todos")
+    public List<Estudiante> obtenerEstudiantes(){
+        return estudiantes;
+    }
 }
